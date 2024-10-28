48e0d60d1a3ae5707c576abf450e37970c0b7bde
nfcr-7 <fokourabier@gmail.com>
2024-10-28 13:08:22 -0400

    Update

---
+++ movie-app/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Home from './pages/home/home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path='movie/:id' element={<h1>Movies detail page</h1>}></Route>
+          <Route path='movies/:type' element={<h1>Movies list page</h1>}></Route>
+          <Route path='/*' element={<h1>Error Page</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
